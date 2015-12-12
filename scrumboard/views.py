df60c61e66434a6b2427d796b0578cd76bf64f8f
bodzio <bogumil.chojnowski@gmail.com>
2015-12-12 02:29:38 +0100

    Created generic class-based views

--- scrumboard/views.py
+++ scrumboard/views.py
@@ -1,17 +1,45 @@
-from django.views import generic
+from django.core.urlresolvers import reverse_lazy
+
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from scrumboard.models import Item
 
 
-class ItemListView(generic.ListView):
-    template_name = 'scrumboard/items_list.html'
+#homepage
+class ScrumboardView(ListView):
+    model = Item
+    template_name = 'scrumboard/scrumboard.html'
     context_object_name = 'all_items'
 
-    def get_queryset(self):
-        return Item.objects.all()
+
+class ItemListView(ListView):
+    model = Item
+    context_object_name = 'all_items'
 
 
-class ItemDetailView(generic.DetailView):
+class ItemDetailView(DetailView):
     model = Item
     context_object_name = 'item'
-    template_name = 'scrumboard/item_details.html'
+
+
+class ItemCreateView(CreateView):
+    model = Item
+    fields = '__all__'
+
+
+class ItemUpdateView(UpdateView):
+    model = Item
+    fields = '__all__'
+
+    def get_object(self, queryset=None):
+        return Item.objects.get(pk=self.kwargs['pk'])
+
+
+class ItemDeleteView(DeleteView):
+    model = Item
+    success_url = reverse_lazy('scrumboard:index')
+
+    def get_object(self, queryset=None):
+        return Item.objects.get(pk=self.kwargs['pk'])

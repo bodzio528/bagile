74f04e92311e147a5bb596b88887cf242e66251a
bodzio <bogumil.chojnowski@gmail.com>
2015-12-12 02:30:27 +0100

    Rewritten urlconfig in scrumboard app to modular

--- scrumboard/urls.py
+++ scrumboard/urls.py
@@ -1,11 +1,20 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
 
-# in template {% url 'scrumboard:detail' item.id %}
+# in template: {% url 'scrumboard:detail' item.id %}
+# in python: reverse('scrumboard:detail', ...)
 app_name = 'scrumboard'
 
 urlpatterns = [
-    url(r'^$', views.ItemListView.as_view(), name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', views.ItemDetailView.as_view(), name='detail'),
+    url(r'^$', views.ScrumboardView.as_view(), name='index'),
+    url(r'^item/', include([
+        url(r'^$', views.ItemListView.as_view(), name='item_list'),
+        url(r'^create/$', views.ItemCreateView.as_view(), name='item_create'),
+        url(r'^(?P<pk>[0-9]+)/', include([
+            url(r'^$', views.ItemDetailView.as_view(), name='item_details'),
+            url(r'^update/$', views.ItemUpdateView.as_view(), name='item_update'),
+            url(r'^delete/$', views.ItemDeleteView.as_view(), name='item_delete'),
+        ])),
+    ]))
 ]

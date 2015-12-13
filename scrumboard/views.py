d0157ec7a9e555f741ded4e7d0f44756ae9211c6
bodzio <bogumil.chojnowski@gmail.com>
2015-12-13 22:59:20 +0100

    Docstrings in views classes added

--- scrumboard/views.py
+++ scrumboard/views.py
@@ -1,35 +1,48 @@
 from django.core.urlresolvers import reverse_lazy
-
-from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
 
 from scrumboard.models import Item
 
 
-#homepage
 class ScrumboardView(ListView):
+    """
+    This View will combine current sprint scrumboard with data like charts, statistics.
+    """
     model = Item
     template_name = 'scrumboard/scrumboard.html'
     context_object_name = 'all_items'
 
 
 class ItemListView(ListView):
+    """
+    Subject to be removed.
+    """
     model = Item
     context_object_name = 'all_items'
 
 
 class ItemDetailView(DetailView):
+    """
+    Detailed information about the Item.
+    """
     model = Item
     context_object_name = 'item'
 
 
 class ItemCreateView(CreateView):
+    """
+    Create new Item instance in separate form.
+    """
     model = Item
     fields = '__all__'
 
 
 class ItemUpdateView(UpdateView):
+    """
+    Update Item instance in separate form.
+    """
     model = Item
     fields = '__all__'
 
@@ -38,6 +51,9 @@ class ItemUpdateView(UpdateView):
 
 
 class ItemDeleteView(DeleteView):
+    """
+    Remove selected Item from application
+    """
     model = Item
     success_url = reverse_lazy('scrumboard:index')
 

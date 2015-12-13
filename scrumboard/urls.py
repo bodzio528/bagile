e975b659ab0ec85c24d991b55213d739c0bf94b8
bodzio <bogumil.chojnowski@gmail.com>
2015-12-13 22:44:49 +0100

    Introduced base template in scrumboard app

--- scrumboard/urls.py
+++ scrumboard/urls.py
@@ -1,20 +1,20 @@
 from django.conf.urls import url, include
 
-from . import views
+from scrumboard.views import *
 
 # in template: {% url 'scrumboard:detail' item.id %}
 # in python: reverse('scrumboard:detail', ...)
 app_name = 'scrumboard'
 
 urlpatterns = [
-    url(r'^$', views.ScrumboardView.as_view(), name='index'),
+    url(r'^$', ScrumboardView.as_view(), name='index'),
     url(r'^item/', include([
-        url(r'^$', views.ItemListView.as_view(), name='item_list'),
-        url(r'^create/$', views.ItemCreateView.as_view(), name='item_create'),
+        url(r'^$', ItemListView.as_view(), name='item_list'),
+        url(r'^create/$', ItemCreateView.as_view(), name='item_create'),
         url(r'^(?P<pk>[0-9]+)/', include([
-            url(r'^$', views.ItemDetailView.as_view(), name='item_details'),
-            url(r'^update/$', views.ItemUpdateView.as_view(), name='item_update'),
-            url(r'^delete/$', views.ItemDeleteView.as_view(), name='item_delete'),
+            url(r'^$', ItemDetailView.as_view(), name='item_details'),
+            url(r'^update/$', ItemUpdateView.as_view(), name='item_update'),
+            url(r'^delete/$', ItemDeleteView.as_view(), name='item_delete'),
         ])),
     ]))
 ]

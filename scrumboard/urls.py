eef7f9b3513873f55a350298c2b5a502b0d5316b
bodzio <bogumil.chojnowski@gmail.com>
2015-12-16 01:17:54 +0100

    SprintPlanningView implemented in raw mode
    Update urlconfig, README and templates

--- scrumboard/urls.py
+++ scrumboard/urls.py
@@ -12,9 +12,19 @@ urlpatterns = [
         url(r'^$', ItemListView.as_view(), name='item_list'),
         url(r'^create/$', ItemCreateView.as_view(), name='item_create'),
         url(r'^(?P<pk>[0-9]+)/', include([
-            url(r'^$', ItemDetailView.as_view(), name='item_details'),
+            url(r'^details/$', ItemDetailView.as_view(), name='item_details'),
             url(r'^update/$', ItemUpdateView.as_view(), name='item_update'),
             url(r'^delete/$', ItemDeleteView.as_view(), name='item_delete'),
         ])),
-    ]))
+    ])),
+    url(r'^sprint/', include([
+        url(r'^create/$', SprintCreateView.as_view(), name='sprint_create'),
+        url(r'^planning/$', SprintPlanningView.as_view(), name='current_sprint_planning'),
+        url(r'^(?P<pk>[0-9]+)/', include([
+            url(r'^details/$', SprintDetailView.as_view(), name='sprint_details'),
+            url(r'^update/$', SprintUpdateView.as_view(), name='sprint_update'),
+            url(r'^delete/$', SprintDeleteView.as_view(), name='sprint_delete'),
+            url(r'^planning/$', SprintPlanningView.as_view(), name='sprint_planning')
+        ])),
+    ])),
 ]
--- scrumboard/views.py
+++ scrumboard/views.py
@@ -2,8 +2,9 @@ from django.core.urlresolvers import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
+from extra_views import InlineFormSetView
 
-from scrumboard.models import Item
+from scrumboard.models import Item, Sprint
 
 
 class ScrumboardView(ListView):
@@ -28,7 +29,6 @@ class ItemDetailView(DetailView):
     Detailed information about the Item.
     """
     model = Item
-    context_object_name = 'item'
 
 
 class ItemCreateView(CreateView):
@@ -47,15 +47,80 @@ class ItemUpdateView(UpdateView):
     fields = '__all__'
 
     def get_object(self, queryset=None):
-        return Item.objects.get(pk=self.kwargs['pk'])
+        return Item.objects.get(id=self.kwargs['pk'])
 
 
 class ItemDeleteView(DeleteView):
     """
-    Remove selected Item from application
+    Item delete
+    @get:   display confirmation
+    @post:  effectively delete
     """
     model = Item
     success_url = reverse_lazy('scrumboard:index')
 
     def get_object(self, queryset=None):
-        return Item.objects.get(pk=self.kwargs['pk'])
+        return Item.objects.get(id=self.kwargs['pk'])
+
+
+class SprintCreateView(CreateView):
+    """
+    Create Sprint as non admin user.
+    """
+    model = Sprint
+    fields = '__all__'
+
+
+class SprintUpdateView(UpdateView):
+    """
+    Update Sprint data.
+    """
+    model = Sprint
+    fields = '__all__'
+
+    def get_object(self, queryset=None):
+        return Sprint.objects.get(id=self.kwargs['pk'])
+
+
+class SprintDetailView(DetailView):
+    """
+    Detailed information about the Sprint.
+    """
+    model = Sprint
+    fields = '__all__'
+
+    def get_object(self, queryset=None):
+        return Sprint.objects.get(id=self.kwargs['pk'])
+
+
+class SprintDeleteView(DeleteView):
+    """
+    Sprint delete
+    @get:   display confirmation
+    @post:  effectively delete
+    """
+    model = Sprint
+    success_url = reverse_lazy('scrumboard:index')
+
+    def get_object(self, queryset=None):
+        return Sprint.objects.get(id=self.kwargs['pk'])
+
+
+class SprintPlanningView(InlineFormSetView):
+    """
+    SprintPlanning a form to add new items to existing sprint.
+
+    Sprint is auto-detected from session or current time.
+    """
+    model = Sprint
+    inline_model = Item
+    # context_object_name = 'sprint'
+    template_name = 'scrumboard/sprint_planning.html'
+
+    def get_object(self):
+        if 'pk' in self.kwargs:
+            return Sprint.objects.get(id=self.kwargs['pk'])
+        elif 'current_sprint' in self.request.session:
+            return self.request.session['current_sprint']
+        else:
+            return Sprint.get_current_sprint()

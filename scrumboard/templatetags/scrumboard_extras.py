c721c425d6f1399d87a4a6f1c692ae7139e6d7f1
bodzio <bogumil.chojnowski@gmail.com>
2016-01-09 12:46:51 +0100

    remove bootstrap-spinner as temporary workaround

--- scrumboard/templatetags/scrumboard_extras.py
+++ scrumboard/templatetags/scrumboard_extras.py
@@ -29,10 +29,3 @@ def item_url(item, text=None):
 @register.inclusion_tag('scrumboard/extras/sprint_text_url.html')
 def sprint_url(sprint):
     return {'sprint': sprint}
-
-
-@register.inclusion_tag('scrumboard/extras/spinner_field.html')
-def bootstrap_spinner(field):
-    field.field.widget.attrs.update({'class': 'form-control'})
-    field.field.widget.input_type = 'text'
-    return {'field': field}

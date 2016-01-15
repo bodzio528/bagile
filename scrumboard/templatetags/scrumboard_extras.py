0ce63db72d96e69b7c0e36e679581d0d88e0d135
bodzio <bogumil.chojnowski@gmail.com>
2016-01-15 22:41:07 +0100

    rewrite templatetags to more common sense names

--- scrumboard/templatetags/scrumboard_extras.py
+++ scrumboard/templatetags/scrumboard_extras.py
@@ -4,22 +4,22 @@ register = template.Library()
 
 
 @register.inclusion_tag('scrumboard/extras/item_form.html')
-def fancy_item_form(form):
+def scrumboard_item_form(form):
     return {'form': form}
 
 
 @register.inclusion_tag('scrumboard/extras/item_full.html')
-def fancy_item_detail(item):
+def scrumboard_item_full(item):
     return {'item': item}
 
 
 @register.inclusion_tag('scrumboard/extras/item_short.html')
-def item_detail_short(item):
+def scrumboard_item_short(item):
     return {'item': item}
 
 
 @register.inclusion_tag('scrumboard/extras/item_text_url.html')
-def item_url(item, text=None):
+def scrumboard_item_url(item, text=None):
     return {
         'item': item,
         'text': text or item.name
@@ -27,5 +27,5 @@ def item_url(item, text=None):
 
 
 @register.inclusion_tag('scrumboard/extras/sprint_text_url.html')
-def sprint_url(sprint):
+def scrumboard_sprint_url(sprint):
     return {'sprint': sprint}
